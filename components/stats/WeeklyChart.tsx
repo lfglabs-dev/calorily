cb3f698666f2591e1b48302f25776d60e773978d
Thomas Marchand <thomas.marchand@tuta.io>
2023-12-28 22:36:58 +0100

    feat: add message if no weekly calories

--- components/stats/WeeklyChart.tsx
+++ components/stats/WeeklyChart.tsx
@@ -1,12 +1,26 @@
-import { Text, View, useColorScheme } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, useColorScheme } from "react-native";
 import { Bar, CartesianChart } from "victory-native";
 import { LinearGradient, useFont, vec } from "@shopify/react-native-skia";
 import { useMealsDatabase } from "../../shared/MealsStorageContext";
 import { getDailyCalories, totalCalories } from "../../utils/food";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+const styles = (scheme) =>
+  StyleSheet.create({
+    icon: {
+      marginBottom: 10,
+      color: scheme === "dark" ? "#A9A9A9" : "#606060",
+    },
+    text: {
+      color: scheme === "dark" ? "#FFF" : "#000",
+      fontSize: 18,
+    },
+  });
 
 const WeeklyCalories = () => {
   const font = useFont(require("../../assets/SFProText-Regular.ttf"), 12);
-
+  const scheme = useColorScheme();
   const { weeklyMeals } = useMealsDatabase();
   const dailyCalories = getDailyCalories(weeklyMeals);
 
@@ -28,7 +42,6 @@ const WeeklyCalories = () => {
     calorieCount: dailyCalories[date] ? totalCalories(dailyCalories[date]) : 0,
   }));
 
-  const scheme = useColorScheme();
   const lightLinesColor = "gray";
   const linesColor = "gray";
 
@@ -44,53 +57,71 @@ const WeeklyCalories = () => {
       >
         Weekly calories in
       </Text>
-      <View
-        style={{
-          borderRadius: 10,
-          backgroundColor: scheme === "dark" ? "#1C1C1E" : "#FFF",
-          paddingLeft: 7,
-          paddingRight: 12,
-          paddingBottom: 6,
-          paddingTop: 15,
-          width: "100%",
-          height: 200,
-        }}
-      >
-        <CartesianChart
-          data={data}
-          xKey="date"
-          yKeys={["calorieCount"]}
-          domainPadding={{ left: 50, right: 50, top: 30 }}
-          axisOptions={{
-            lineColor: scheme === "dark" ? linesColor : lightLinesColor,
-            labelColor: scheme === "dark" ? linesColor : lightLinesColor,
-            font,
-            formatXLabel(value) {
-              const date = new Date(value);
-              return date
-                .toLocaleDateString("en-US", { weekday: "long" })
-                .split(",")[0];
-            },
+
+      {weeklyMeals.length === 0 ? (
+        <View
+          style={{
+            borderRadius: 10,
+            width: "100%",
+            height: 200,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: scheme === "dark" ? "#1C1C1E" : "#FFF",
           }}
         >
-          {({ points, chartBounds }) => (
-            <Bar
-              chartBounds={chartBounds}
-              points={points.calorieCount}
-              roundedCorners={{
-                topLeft: 5,
-                topRight: 5,
-              }}
-            >
-              <LinearGradient
-                start={vec(0, 0)}
-                end={vec(0, 400)}
-                colors={["#a78bfa", "#1E8BFA00"]}
-              />
-            </Bar>
-          )}
-        </CartesianChart>
-      </View>
+          <FontAwesome name="bar-chart" size={40} style={styles(scheme).icon} />
+          <Text style={styles(scheme).text}>No data available</Text>
+        </View>
+      ) : (
+        <View
+          style={{
+            borderRadius: 10,
+            backgroundColor: scheme === "dark" ? "#1C1C1E" : "#FFF",
+            paddingLeft: 7,
+            paddingRight: 12,
+            paddingBottom: 6,
+            paddingTop: 15,
+            marginBottom: 0,
+            width: "100%",
+            height: 200,
+          }}
+        >
+          <CartesianChart
+            data={data}
+            xKey="date"
+            yKeys={["calorieCount"]}
+            domainPadding={{ left: 50, right: 50, top: 30 }}
+            axisOptions={{
+              lineColor: scheme === "dark" ? linesColor : lightLinesColor,
+              labelColor: scheme === "dark" ? linesColor : lightLinesColor,
+              font,
+              formatXLabel(value) {
+                const date = new Date(value);
+                return date
+                  .toLocaleDateString("en-US", { weekday: "long" })
+                  .split(",")[0];
+              },
+            }}
+          >
+            {({ points, chartBounds }) => (
+              <Bar
+                chartBounds={chartBounds}
+                points={points.calorieCount}
+                roundedCorners={{
+                  topLeft: 5,
+                  topRight: 5,
+                }}
+              >
+                <LinearGradient
+                  start={vec(0, 0)}
+                  end={vec(0, 400)}
+                  colors={["#a78bfa", "#1E8BFA00"]}
+                />
+              </Bar>
+            )}
+          </CartesianChart>
+        </View>
+      )}
     </>
   );
 };
